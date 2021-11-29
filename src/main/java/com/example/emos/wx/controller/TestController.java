ae9d72a626a4f1b92bfe189720597996dac46657
silenceallat <846837823@qq.com>
2021-11-29 17:40:05 +0800

    更新项目代码

---
+++ src/main/java/com/example/emos/wx/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.emos.wx.controller;
+
+
+import com.example.emos.wx.common.util.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/test")
+@Api("测试web接口")
+public class TestController {
+    @PostMapping("/sayhello")
+    @ApiOperation(value = "简单的测试方法")
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form){
+        return R.ok().put("message","hello swagger"+form.getName());
+    }
+}
