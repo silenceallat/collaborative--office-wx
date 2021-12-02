985efc704b4e1b3c9ec7155fa5bf715319846344
silenceallat <846837823@qq.com>
2021-12-02 17:02:18 +0800

    更新

---
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Fitler.java
@@ -0,0 +1,82 @@
+package com.example.emos.wx.config.shiro;/*
+ * @ClassName:OAuth2Fitler
+ * @author: song
+ * @date:2021/12/2
+ */
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Scope("prototype")
+public class OAuth2Fitler extends AuthenticatingFilter {
+    @Autowired
+    private ThreadLocalToken threadLocalToken;
+    @Value("${emos.jwt.cache-expire}")
+    private int cacheExpire;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /*
+     * 拦截请求后，用于把令牌字符串封装成令牌对象
+     * */
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
+        String token = getRequesToken((HttpServletRequest) request);
+        if (StringUtils.isBlank(token)) {
+            return null;
+        }
+        return new OAuth2Token(token);
+    }
+
+    /*
+     * 拦截请求，判断请求是否需要被Shiro处理
+     * */
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        HttpServletRequest req = (HttpServletRequest) request;
+        // Ajax提交application/json数据的时候，会先发出Options请求
+        // 这里要放行Options请求，不需要Shiro处理
+        if (req.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            return true;
+        }
+        // 除了Options请求之外，所有请求都要被Shiro处理
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
+        return false;
+    }
+
+    private String getRequesToken(HttpServletRequest httpRequest) {
+        //    从header 中获取token
+        String token = httpRequest.getHeader("token");
+        //如果header中不存在token，则从参数中获取token
+        if (StringUtils.isBlank(token)) {
+            token = httpRequest.getParameter("token");
+        }
+        return token;
+    }
+
+    @Override
+    public void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        super.doFilterInternal(request, response, chain);
+    }
+}
